80ddd415602f8aedc112890f101e6d6b1b65c7e2
Sohan Rajpurohit <121212504+SohanRazz@users.noreply.github.com>
2023-01-20 11:04:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/Netflix_mdisk_bot.git /Netflix_mdisk_bot
+git clone https://github.com/Sohanrazz/Netflix_mdisk_bot.git /Netflix_mdisk_bot
 cd /Netflix_mdisk_bot
 pip3 install -U -r requirements.txt
 echo "⚡️⚡️ Starting LazyUrlHunterrBOT..."
